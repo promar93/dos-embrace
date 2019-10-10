18316a893ece0e5a2c9d9bc5bafdadc034c127c5
Omar Forrester <omar@downBridge.com>
2019-10-10 13:34:44 -0400

    npx init

--- src/index.js
+++ src/index.js
@@ -1,4 +1,13 @@
 import React from "react"
-import ReactDOM from "react-dom"
+// import ReactDOM from "react-dom"
 
-ReactDOM.render()
+// ReactDOM.render()
+class HelloMessage extends React.Component {
+    render() {
+        return (
+            <h1>Hello {this.props.name}</h1>
+        )
+    }
+}
+
+export default HelloMessage
